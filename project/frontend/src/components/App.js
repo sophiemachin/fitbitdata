d6f1e01b3ef29f50e4534e3df0bce4c6c82e5522
Sophie Machin <sophiemachin@gmail.com>
2019-03-09 13:02:01 +0000

    remove tutorial files

--- project/frontend/src/components/App.js
+++ project/frontend/src/components/App.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
-import WeightTable from "./Table";
 import APlot from "./PlotlyStuff";
-// import AForm from "./Form";
 
 const App = () => (
 <div>
-  <DataProvider endpoint="api/lead/" 
-                render={data => <Table data={data} />} />
-                
 
   <DataProvider endpoint="api/weight/" 
                 render={data => <APlot data={data} />} />
