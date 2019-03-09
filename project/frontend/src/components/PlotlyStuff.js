f85d9d55554fbb87615ec29e1ce0064f4244e172
Sophie Machin <sophiemachin@gmail.com>
2019-03-09 12:56:16 +0000

    more weight data

--- project/frontend/src/components/PlotlyStuff.js
+++ project/frontend/src/components/PlotlyStuff.js
@@ -1,21 +1,55 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
+
+function formatData(data) {
+
+  let x = 1
+
+
+  var formatted = {
+    'x' : [],
+    'y': [],
+    type: 'scatter',
+    'x0' : 'date',
+    mode: 'lines+points',
+    marker: {color: 'red'},
+  }
+
+  for (let date of data) {
+    // console.log(date)
+
+    formatted.x.push(date.date)
+    formatted.y.push(date.weight)
+    x = x + 1
+  }
+
+  return formatted
+
+}
+
 class APlot extends React.Component {
   render() {
+
+    const formattedData = formatData(this.props.data)
+
     return (
       <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+points',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 640, height: 480, title: 'This is my sample plot'} }
+        data={[formattedData]}
+
+        layout={{
+          width: 640,
+          height: 480,
+          title: 'Weight data - Feb',
+          xaxis:
+            {
+              title:'Day',
+            },
+          yaxis:
+            {
+              title:'Weight (lbs)',
+            }
+        }}
       />
     );
   }
---
+++ project/leads/fixtures/weight/convert_weight.py
@@ -0,0 +1,38 @@
+import json
+import datetime
+import glob, os
+
+converted = []
+
+pk = 1
+
+
+os.chdir(".")
+
+for f in glob.glob("*.json"):
+
+	with open(f) as f:
+		data = json.load(f)
+
+		for item in data:
+
+			date_obj = datetime.datetime.strptime(item['date'], '%m/%d/%y')
+			formated_date = datetime.datetime.strftime(date_obj, '%Y-%m-%d')
+			item['date'] = formated_date
+
+			del item["time"]
+			new_obj = {
+				'model' : 'leads.weight',
+				'pk' : pk,
+				'fields' : item,
+		 	}
+
+			pk = pk + 1
+
+			converted.append(new_obj)
+
+
+
+with open("../weight.json", 'w') as f:
+	json.dump(converted, f)
+
